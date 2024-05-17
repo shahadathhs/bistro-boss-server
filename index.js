e27075ae9e8e89a6d06df3dd5769ee8b437d8026
Shahadath Hossen Sajib <shahadathhossensajib732@gmail.com>
2024-05-17 06:34:42 +0600

    basic server and mongo setup completed

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const user = process.env.DB_USER
+const password = process.env.DB_PASS
+
+const uri = `mongodb+srv://${user}:${password}@cluster0.ahaugjj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    //await client.connect();
+    // Send a ping to confirm a successful connection
+    //await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Bistro Boss Server Running!')
+})
+
+app.listen(port, () => {
+  console.log(`Bistro Boss listening on port ${port}`)
+})
