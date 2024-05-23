07598c60f1bab53c95f35916933d6fd44ca2be57
Shahadath Hossen Sajib <shahadathhossensajib732@gmail.com>
2024-05-23 08:05:11 +0600

    token created

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config();
+const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173"
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser())
 
 const user = process.env.DB_USER
 const password = process.env.DB_PASS
@@ -23,6 +33,29 @@ const client = new MongoClient(uri, {
   }
 });
 
+const verifyToken = async(req, res, next) => {
+  const token = req.cookies?.token;
+  //console.log("middleware token", token)
+  // token unavailable
+  if (!token) {
+    return res.status(401).send({message: "Unauthorized"})
+  }
+  // token available
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+    if(error){
+      return res.status(403).send({message: "Forbidden"})
+    }
+    req.decodedToken = decoded;
+    next();
+  })
+}
+
+const cookieOptions = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production" ? true : false,
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -33,6 +66,27 @@ async function run() {
     const reviewsCollection = database.collection("reviews");
     const cartsCollection = database.collection("carts");
     const usersCollection = database.collection('users');
+
+    //jwt related api
+    //creating Token
+    app.post("/jwt", async (req, res) => {
+      const userEmail = req.body;
+      console.log("user for token", userEmail);
+      const token = jwt.sign(userEmail, process.env.ACCESS_TOKEN_SECRET);
+
+      res
+        .cookie("token", token, cookieOptions)
+        .send({ loginSuccess: true, token: token });
+    });
+
+    //clearing Token
+    app.post("/logout", async (req, res) => {
+      const userEmail = req.body;
+      console.log("logging out", userEmail);
+      res
+        .clearCookie("token", { ...cookieOptions, maxAge: 0 })
+        .send({ logoutSuccess: true });
+    });
 
     // users related api
     app.get("/users", async(req, res) => {
@@ -69,6 +123,7 @@ async function run() {
         },
       };
       const result = await usersCollection.updateOne(query, updateDoc);
+      res.send(result)
     })
     
     // menu related api
