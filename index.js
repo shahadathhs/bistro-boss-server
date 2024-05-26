1ef3e8146cb820d241f8157ac3db01f4127efb49
Shahadath Hossen Sajib <shahadathhossensajib732@gmail.com>
2024-05-26 21:55:38 +0600

    payment intent api added

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
-require('dotenv').config()
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -299,6 +300,23 @@ async function run() {
       const query = { _id : new ObjectId(id)};
       const result = await cartsCollection.deleteOne(query);
       res.send(result)
+    })
+
+    //payment intent
+    app.post("/create-payment-intent", async (req, res) => {
+      const {price} = req.body;
+      const amount = parseInt(price * 100);
+
+      // Create a PaymentIntent with the order amount and currency
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card", "link"],
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
     })
 
     // Send a ping to confirm a successful connection
